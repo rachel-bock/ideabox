2d5b3dc4504e93a95fe82f9fe5a6c2195ed58562
Irmak Erdem <irmakerdem9@gmail.com>
2022-04-13 11:38:37 -0600

    Refactor HTML and add displayCard functionality

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var list = [];
 var saveButton = document.querySelector('.save-button');
 var inputTitle = document.querySelector('#title');
 var inputBody = document.querySelector('#body');
+var cardGrid = document.querySelector('.card-section-grid');
 
 //EVENT LISTENERS
 saveButton.addEventListener('click', processInput);
@@ -16,9 +17,23 @@ function processInput() {
   displayCard();
 }
 
-
-
 //DOM FUNCTIONS
 function displayCard() {
-
+  cardGrid.innerHTML = "";
+  for (var i = 0; i < list.length; i++) {
+    cardGrid.innerHTML += `<div class="card">
+      <div class="card-top">
+        <button class="favorite"></button>
+        <button class="delete"></button>
+      </div>
+      <div class="card-middle">
+        <h1>${list[i].title}</h1>
+        <p>${list[i].body}</p>
+      </div>
+      <div class="card-bottom">
+        <button class="comment"></button>
+        <p>Comment</p>
+      </div>
+    </div>`;
+  }
 }
