43d147e6f5097851911c16a3a9026d0470a16cfb
Irmak Erdem <irmakerdem9@gmail.com>
2022-04-13 10:58:36 -0600

    Add processInput functionality

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+//GLOBAL VARIABLES
 var list = [];
+
+//QUERY SELECTORS
+var saveButton = document.querySelector('.save-button');
+var inputTitle = document.querySelector('#title');
+var inputBody = document.querySelector('#body');
+
+//EVENT LISTENERS
+saveButton.addEventListener('click', processInput);
+
+//DATA FUNCTIONS
+function processInput() {
+  var newIdea = new Idea(inputTitle.value, inputBody.value);
+  list.push(newIdea);
+  displayCard();
+}
+
+
+
+//DOM FUNCTIONS
+function displayCard() {
+
+}
+
+
+// 2.1 When I click “Save”,
+// - If I entered information in both the “Title” and “Body” input fields,
+// - I should see a new Idea instance with the provided title and body appear in the ideas array
+// - I should see a new idea card with the provided title and body appear on the DOM
