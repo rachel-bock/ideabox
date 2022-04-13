37a33589dd5f4385dac454fe45c125556691f704
Irmak Erdem <irmakerdem9@gmail.com>
2022-04-13 11:11:18 -0600

    Remove pseudocode

--- main.js
+++ main.js
@@ -22,9 +22,3 @@ function processInput() {
 function displayCard() {
 
 }
-
-
-// 2.1 When I click “Save”,
-// - If I entered information in both the “Title” and “Body” input fields,
-// - I should see a new Idea instance with the provided title and body appear in the ideas array
-// - I should see a new idea card with the provided title and body appear on the DOM
